b74b4d552a5a03bf9ce157018c76274c1e6a6ebb
jammiesymondstayler@gmail.com <jammiesymondstayler@gmail.com>
2020-10-19 17:57:13 +0100

    user status logic and control, list indexing, cors access, port use

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,35 @@
 class ListsController < ApplicationController
+
+
+    def index
+        all_lists = current_user.lists.map{|list|
+            {
+                name: list.name,
+                id:list.id,
+                icon:list.icon,
+            }
+        }
+        render json:{all_lists: all_lists}, status: :ok
+    end
+
+    def show
+        list = List.find_by(id: params[:id])
+        if !list
+            render json:{error: "list not found"}, status: :not_found
+        elsif !list.users.any?{|user| user.id== current_user.id}
+            render json: {error: "not authorized"}, status: :unauthorized
+        else
+            list_hash ={
+                name: list.name,
+                list: list.id,
+                icon: list.icon,
+                items: list.items.map{|item| {
+                    name: item.name,
+                    category: item.category,
+                }}
+            }
+            render json: list_hash, status: :ok
+        end
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+jamie = User.create(username: "jamie", password: "jamie")
+
+puts "created Jamie"
+
+list_one = List.create(name: "Example List")
+
+Right.create(user:jamie, list: list_one)
+
+puts "created list one"
+
+list_one_items = [
+    {name: 'burgers', category: 'Meat'}, 
+    {name: 'sausages', category: 'Meat'}, 
+    {name: 'burger buns', category: 'Bread'}, 
+    {name: 'lettuce', category: 'Produce'},
+    {name: 'tomatoes', category: 'Produce'},
+]
+
+list_one_items.each{|item|
+    Item.create(list: list_one, name:item[:name], category:item[:category])
+}
+
+puts "added items to list one."
