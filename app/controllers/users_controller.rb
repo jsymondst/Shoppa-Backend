c2d3b91f69f640488a849aab3af8f05fc99f7209
jammiesymondstayler@gmail.com <jammiesymondstayler@gmail.com>
2020-10-19 13:06:52 +0100

    initial routing

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+
+    skip_before_action :authorized, only: [:create, :login]
+
+    def login
+        @user = User.find_by(username: user_login_params[:username])
+        if @user && @user.authenticate(user_login_params[:password])
+            payload = encode_token({ user_id: @user.id, username: @user.username })
+            puts payload
+            render json: { user: UserSerializer.new(@user), jwt: payload }, status: :accepted
+        else
+            render json: { message: "Invalid username or password" }, status: :unauthorized
+        end
+    end
+
+    def create
+        @user = User.new(user_login_params)
+        if @user.save
+            payload = encode_token({user_id: @user.id, username: @user.username})
+            render json: { user: UserSerializer.new(@user), jwt:payload}, status: :accepted
+        else
+            render json: {message: "User not created"}, status: :conflict
+        end
+    end
+
+    def checkin
+        puts current_user
+        
+        render json: {user: UserSerializer.new(current_user)}
+    end
+
+    private
+
+    def user_login_params
+        params.require(:user).permit(:username, :password)
+    end
+
+    def user_create_params
+        params.require(:user).permit(:username, :password)
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:create]
+
+  post '/login/', to: "users#login"
+  get '/checkin/', to: "users#checkin"
+
+  get '/lists/', to: 'lists#index'
+  post '/lists/', to: 'lists#create'
+  delete '/lists/:id', to: 'list#delete'
+  patch 'lists/:id', to: 'lists#update'
+
 end
